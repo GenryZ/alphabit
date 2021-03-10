1e77ec2c38d8bfff9d0f514dcbff4f6e5483b115
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-03-10 15:33:23 +0300

    initial commit

---
+++ alphabet.rb
@@ -0,0 +1,13 @@
+ha = "["
+tw = "]"
+dje = ";"
+ee = "'"
+be = ","
+you = "."
+str = "23124l345fnf4362hj;2134ltyb234z"
+str_ing = str.split(//)
+str_ing.each do |x|
+if x == ";" 
+puts "æ"
+end
+end
---
+++ app.rb
@@ -0,0 +1,9 @@
+str = "23124l345fnf4362hj;2134ltyb234z"
+
+a = str.split(//)
+a.each do |x|
+if x.to_i == 0
+  print x
+end
+end
+ 
