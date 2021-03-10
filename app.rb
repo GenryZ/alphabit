1a1f97618fd25d7b2fabc047bb90119f96fb2668
GenryZ <genrymorganz@gmail.com>
2021-03-10 15:49:14 +0300

    Add symbols

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
-str = "23124l345fnf4362hj;2134ltyb234z"
-
-a = str.split(//)
-a.each do |x|
-if x.to_i == 0
-  print x
+ha = "[" 
+tw = "]"
+dje = ";"
+ee = "'"
+be = ","
+you = "."
+puts "Введите ваши символы"
+input = gets.strip
+str = input
+str_ing = str.split(//)
+str_ing.each do |x|
+if x == "["
+puts "х"
+elsif x == "]"
+	puts "ъ"
+elsif x == ";"
+	puts "ж"
+elsif x == "'"
+	puts "э"
+elsif x == ","
+	puts "б"
+elsif x == "."
+	puts "ю"
 end
 end
- 
